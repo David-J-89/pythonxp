e9bd49b02a92c6a56fa1727d798d8624e4f3634f
David-J-89 <davidwhaleycode@outlook.com>
2018-09-12 12:11:00 -0500

    init

---
+++ textrpg/game.py
@@ -0,0 +1,24 @@
+# Python Text RPG
+# davidj
+
+import cmd
+import textwrap
+import sys
+import os
+import time
+import random
+
+screen_width = 100
+
+##### Player Setup #####
+
+
+class player:
+    def __init__(self):
+        self.name = ''
+        self.hp = 0
+        self.mp = 0
+        self.status_effects = []
+
+
+myPlayer = player()
