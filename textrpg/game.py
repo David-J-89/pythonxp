92bff789f6b8bf8213c67df069770bf3bbf8c2ec
David-J-89 <davidwhaleycode@outlook.com>
2018-09-12 12:26:55 -0500

    base_title

--- textrpg/game.py
+++ textrpg/game.py
@@ -22,3 +22,35 @@ class player:
 
 
 myPlayer = player()
+
+##### Title Screen #####
+
+
+def title_screen_selections():
+    option = input("> ")
+    if option.lower() == ("play"):
+        start_game()  # placeholder until written
+    elif option.lower() == ("help"):
+        help_menu()
+    elif option.lower() == ("quit"):
+        sys.exit()
+    while option.lower() not in ('play', 'help', 'quit'):
+        print("Please enter a valid command.")
+        option = input("> ")
+        if option.lower() == ("play"):
+            start_game()  # placeholder until written
+        elif option.lower() == ("help"):
+            help_menu()
+        elif option.lower() == ("quit"):
+            sys.exit()
+
+
+def title_screen():
+    os.system('clear')
+    print('############################')
+    print('# Welcome to the Text RPG! #')
+    print('         - Play -           ')
+    print('         - Help -           ')
+    print('         - Quit -           ')
+    print('   Copyright 2018 davidj    ')
+    title_screen_selections()
